1a9ed577996e1bb288665ca70a4808e13354074e
lixxce5017 <lixxce5017@gmail.com>
2025-09-12 01:37:10 +0900

    refator: 컨테이너 분리를 위한 기존 모듈들 삭제

--- src/main/java/core/global/search/dto/PostCreatedEvent.java
+++
@@ -1,6 +0,0 @@
-package core.global.search.dto;
-
-import core.domain.post.entity.Post;
-
-public record PostCreatedEvent(Post post) {
-}
--- src/main/java/core/global/service/SchedulerConfig.java
+++
@@ -1,8 +0,0 @@
-package core.global.service;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulerConfig {}
